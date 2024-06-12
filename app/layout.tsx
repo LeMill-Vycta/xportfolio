cc07bdafd731544d859a9bd8fb53ca9ae40131d6
LeMill-Vycta <128823842+LeMill-Vycta@users.noreply.github.com>
2024-06-12 10:53:20 -0300

    awesome template

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "X Portfolio",
+  description: "Modern & Minimal JS Portfolio Template",
 };
 
 export default function RootLayout({
